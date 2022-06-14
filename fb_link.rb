79a7166ce5e43a431d91423e34146251c3f8395a
siery <siery@comic.com>
2022-06-15 01:15:05 +0200

    Skip symbol '%' from being compared each time

--- fb_link.rb
+++ fb_link.rb
@@ -14,20 +14,20 @@ if full_link[0..PREFIX_L] != 'https://l.facebook.com/l.php?u='
 end
 
 full_link = full_link[PREFIX_L + 1..-1]
-replace = {'%3A' => ':', '%2F' => '/', '%3F' => '?', '%3D' => '='}
+replace = {'3A' => ':', '2F' => '/', '3F' => '?', '3D' => '='}
 
 skip = 0
 
 full_link.each_char.with_index do |char, index|
   if char == '%'
-    if full_link[index - skip..index + 2 - skip] == '%26'
+    if full_link[index - skip + 1..index + 2 - skip] == '26'
       full_link = full_link[0..index - skip - 1]
       break
     end
     replace.keys.each do |key|
-      # warn "#{index}: #{full_link[index - skip..index + 2 - skip]} ~ #{key}"
-      if full_link[index - skip..index + 2 - skip] == key
-        # warn skip
+      warn "#{index}: #{full_link[index - skip + 1..index + 2 - skip]} ~ #{key}"
+      if full_link[index - skip + 1..index + 2 - skip] == key
+        warn skip
         full_link[index - skip..index + 2 - skip] = replace[key]
         skip += 2
         break
